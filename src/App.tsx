b2ed0ae821bd39547af2252637264301460fd587
zsheng <zsheng.dev@gmail.com>
2024-12-17 17:50:08 +0800

    add markdown rendering

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { db } from '@/lib/db';
 import { useFlashcardReviewQuery } from '@/lib/query';
 import { gradeCard } from '@/lib/review';
 import { CardWithContent } from '@/lib/types';
+import { markdownToHtml } from '@/lib/utils';
 import { useLiveQuery } from 'dexie-react-hooks';
 import { useMemo } from 'react';
 import { createEmptyCard, Grade } from 'ts-fsrs';
@@ -44,9 +45,20 @@ function App() {
       <h1 className='text-2xl font-bold mb-4'>Review cards</h1>
       <div className='flex flex-wrap gap-2 mb-6 border border-1 shadow-md p-2'>
         {nextReviewCard ? (
-          <div className='w-40 h-80 flex flex-col'>
-            <div>{nextReviewCard.question}</div>
-            <div>{nextReviewCard.answer}</div>
+          <div className='w-40 h-80 flex flex-col justify-between'>
+            <article
+              className='prose'
+              dangerouslySetInnerHTML={{
+                __html: markdownToHtml(nextReviewCard.question),
+              }}
+            ></article>
+            <hr className='my-4' />
+            <article
+              className='prose'
+              dangerouslySetInnerHTML={{
+                __html: markdownToHtml(nextReviewCard.answer),
+              }}
+            ></article>
           </div>
         ) : (
           <div>No cards to review</div>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import rehypeStringify from 'rehype-stringify';
+import remarkGfm from 'remark-gfm';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import { twMerge } from 'tailwind-merge';
+import { unified } from 'unified';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function markdownToHtml(markdown: string) {
+  const result = unified()
+    .use(remarkParse)
+    .use(remarkGfm)
+    .use(remarkRehype)
+    .use(rehypeStringify)
+    .processSync(markdown);
+
+  return result.toString();
 }
