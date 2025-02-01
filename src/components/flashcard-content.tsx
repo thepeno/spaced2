b93a95160342923dcd82af1d9efbab35a0fb685b
zsheng <zsheng.dev@gmail.com>
2025-02-01 12:36:24 -0500

    chore: upgrade tailwind 4.0

--- src/components/flashcard-content.tsx
+++ src/components/flashcard-content.tsx
@@ -3,7 +3,7 @@ import { markdownToHtml } from '@/lib/utils';
 export default function FlashcardContent({ content }: { content: string }) {
   return (
     <article
-      className='prose h-96 flex-1 border border-1 p-2 rounded-sm flex flex-col items-center justify-center shadow-sm w-full'
+      className='prose h-96 flex-1 border border-1 p-2 rounded-sm flex flex-col items-center justify-center shadow-xs w-full'
       dangerouslySetInnerHTML={{
         __html: markdownToHtml(content),
       }}
