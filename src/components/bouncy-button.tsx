38d17eb2c828e11e0af2fd8c35d4b8753c1f1ef8
zsheng <zsheng.dev@gmail.com>
2025-02-02 12:46:59 -0500

    fix: handle drag case for bouncy button
    When the user drags away, the mouseup event is not triggered if the
    mouse is not within the component.
    
    To fix this, we add a global mouseup listener when the button is in the
    pressed state.

--- src/components/bouncy-button.tsx
+++ src/components/bouncy-button.tsx
@@ -1,39 +1,75 @@
 import { cn } from '@/lib/utils';
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function BouncyButton({
   children,
+  variant = 'default',
+  className,
+  pressed,
+  asButton = false,
 }: {
   children: React.ReactNode;
+  variant?: 'default' | 'medium';
+  className?: string;
+  pressed?: boolean;
+  asButton?: boolean;
 }) {
   const [isPressed, setIsPressed] = useState(false);
   const [isReleased, setIsReleased] = useState(false);
 
-  const handlePress = () => {
+  const handlePress = useCallback(() => {
     setIsPressed(true);
     setIsReleased(false);
-  };
+  }, []);
 
-  const handleRelease = () => {
+  const handleRelease = useCallback(() => {
     setIsPressed(false);
     setIsReleased(true);
     // Reset the release state after the animation
     setTimeout(() => setIsReleased(false), 150);
-  };
+  }, []);
+
+  useEffect(() => {
+    if (!isPressed) return;
+
+    const handleMouseUp = () => {
+      handleRelease();
+    };
+
+    document.addEventListener('mouseup', handleMouseUp);
+    return () => document.removeEventListener('mouseup', handleMouseUp);
+  }, [isPressed, handleRelease]);
+
+  // To handle the case where the button is pressed but we drag the mouse away
+  useEffect(() => {
+    if (pressed === undefined) return;
+
+    if (pressed && !isPressed) {
+      handlePress();
+    } else if (!pressed && isPressed) {
+      handleRelease();
+    }
+  }, [pressed, isPressed, handlePress, handleRelease]);
+
+  const Comp = asButton ? 'button' : 'div';
 
   return (
-    <button
+    <Comp
       className={cn(
-        'h-max w-max text-background cursor-pointer transition-transform duration-150 active:duration-100',
+        'h-max w-max cursor-pointer transition-transform duration-150 active:duration-100',
         isPressed && 'scale-90',
-        isReleased && 'scale-105'
+        isReleased && 'scale-105',
+        variant == 'medium' && isPressed && 'scale-95',
+        variant == 'medium' && 'duration-200 active:duration-150',
+        variant == 'medium' && isReleased && 'scale-102',
+        className,
       )}
       onMouseDown={handlePress}
-      onMouseUp={handleRelease}
+      onDragEnd={handleRelease}
       onTouchStart={handlePress}
       onTouchEnd={handleRelease}
     >
       {children}
-    </button>
+    </Comp>
   );
 }
--- src/components/nav/spaced-icon.tsx
+++ src/components/nav/spaced-icon.tsx
@@ -1,4 +1,6 @@
+import BouncyButton from '@/components/bouncy-button';
 import { useReviewCards } from '@/components/hooks/query';
+import MemoryDB from '@/lib/db/memory';
 import { cn } from '@/lib/utils';
 import { Telescope } from 'lucide-react';
 import { Link } from 'react-router';
@@ -7,9 +9,20 @@ export function SpacedIcon() {
   const reviewCards = useReviewCards();
 
   return (
-    <Link to='/'>
+    <Link
+      to='/'
+      onClick={() => {
+        MemoryDB.notify();
+      }}
+      draggable={false}
+    >
       <div className='absolute top-4 md:top-4 left-1/2 -translate-x-1/2 md:translate-x-0 md:left-4 w-16 h-12 z-30 flex items-center justify-center cursor-pointer'>
-        <Telescope className={cn('h-10 w-10')} strokeWidth={1.8} />
+        <BouncyButton>
+          <Telescope
+            className={cn('h-10 w-10 text-primary')}
+            strokeWidth={1.8}
+          />
+        </BouncyButton>
 
         <div className='absolute -bottom-1 -right-2 font-bold text-xs text-muted-foreground rounded-xl bg-muted px-2 sm:hidden'>
           {reviewCards.length}
