e4b2c4f0237a3ccd2143222da34f6fe24591f6fd
zsheng <zsheng.dev@gmail.com>
2025-02-02 15:44:57 -0500

    feat: update radius for return to top

--- src/components/return-to-top.tsx
+++ src/components/return-to-top.tsx
@@ -18,17 +18,24 @@ export default function ReturnToTop() {
   }, []);
 
   return createPortal(
-    <Button
-      className={cn(
-        'fixed top-4 left-1/2 -translate-x-1/2 z-30 rounded-full shadow-sm opacity-100 transition-all scale-100',
-        !isVisible && 'opacity-0 scale-90'
-      )}
-      onClick={() =>
-        document.documentElement.scrollTo({ top: 0, behavior: 'smooth' })
-      }
+    <div
+      // Make the surrounding hit area slighlty bigger
+      className='fixed top-4 left-1/2 -translate-x-1/2 z-30 py-4 px-8'
+      onClick={() => {
+        if (isVisible) {
+          document.documentElement.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+      }}
     >
-      <ArrowUp className='h-4 w-4' />
-    </Button>,
+      <Button
+        className={cn(
+          'rounded-full shadow-sm opacity-100 transition-all scale-100',
+          !isVisible && 'opacity-0 scale-90'
+        )}
+      >
+        <ArrowUp className='h-4 w-4' />
+      </Button>
+    </div>,
     document.body
   );
 }
