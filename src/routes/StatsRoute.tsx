c91a382449cbbee51d3bf75b25583e612c2073e0
zsheng <zsheng.dev@gmail.com>
2025-02-02 22:26:49 -0500

    feat: add loader for stats route

--- src/routes/StatsRoute.tsx
+++ src/routes/StatsRoute.tsx
@@ -6,10 +6,23 @@ import { ReviewChart } from '@/components/stats/review-chart';
 import { db } from '@/lib/db/persistence';
 import { processReviewLogOperations } from '@/lib/review';
 import { useLiveQuery } from 'dexie-react-hooks';
+import { Loader2 } from 'lucide-react';
 
 export default function StatsRoute() {
-  const allReviewLogOperations =
-    useLiveQuery(() => db.reviewLogOperations.toArray()) ?? [];
+  const allReviewLogOperations = useLiveQuery(() =>
+    db.reviewLogOperations.toArray()
+  );
+
+  if (!allReviewLogOperations) {
+    return (
+      <div className='flex flex-col h-full col-start-1 col-end-13 xl:col-start-3 xl:col-end-11 md:px-24 pb-6 gap-2 animate-fade-in'>
+        <div className='flex flex-col h-full min-h-96 justify-center items-center'>
+          <Loader2 className='w-16 h-16 animate-spin text-primary' />
+        </div>
+      </div>
+    );
+  }
+
   const reviewLogs = processReviewLogOperations(allReviewLogOperations);
 
   return (
